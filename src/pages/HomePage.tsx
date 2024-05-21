1771a2747a83fe46e74cfb9cecb3ee68b9a56ff4
dknife <dknifeb@mail.ru>
2024-05-21 19:22:21 +0300

    feat: add Header in HomePage

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,8 +1,11 @@
+import { Header } from "@/components/Header/Header"
 
 export const HomePage = () => {
     return (
         <>
-        
+         <header>
+            <Header/>
+         </header>
         </>
     )
 }
