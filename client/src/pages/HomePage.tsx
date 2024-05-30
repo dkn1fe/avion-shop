6fde7c27ec5b303af915731d188e84b71bc8d03a
dknife <dknifeb@mail.ru>
2024-05-30 19:16:33 +0300

    feat: add sign-up block

--- client/src/pages/HomePage.tsx
+++ client/src/pages/HomePage.tsx
@@ -1,20 +1,23 @@
-import { Header } from "@/components/Header/Header"
-import { MainBlock } from "@/components/MainBlock/MainBlock"
-import { Carusel } from "@/components/caruselBlock/Carusel"
+import { Header } from "@/components/Header/Header";
+import { MainBlock } from "@/components/MainBlock/MainBlock";
+import { Carusel } from "@/components/caruselBlock/Carusel";
+import { SignUp } from "@/components/signUp/SignUp";
 
 export const HomePage = () => {
- 
-    return (
-        <>
-         <header>
-            <Header/>
-         </header>
-         <main className="mt-20">
-            <MainBlock/>
-         </main>
-         <section className="mt-32">
-             <Carusel/>
-         </section>
-        </>
-    )
-}
+  return (
+    <>
+      <header>
+        <Header />
+      </header>
+      <main className="mt-20">
+        <MainBlock />
+      </main>
+      <section className="mt-32">
+        <Carusel />
+      </section>
+      <section className="mt-20">
+        <SignUp />
+      </section>
+    </>
+  );
+};
